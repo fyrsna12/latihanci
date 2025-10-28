7d02c8f95f09d9fda40831448d18eb81fc0264a5
fyrsna <firda20240200119@sibermu.ac.id>
2025-10-28 14:32:03 +0700

    example

---
+++ application/controllers/User.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller 
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$this->load->view('templates/auth_header');
+		$this->load->view('auth/login');
+		$this->load->view('templates/auth_footer');
+	}
+
+	public function register()
+	{
+		$this->load->view('templates/auth_header');
+		$this->load->view('auth/register');
+		$this->load->view('templates/auth_footer');
+	}
+
+	
+}

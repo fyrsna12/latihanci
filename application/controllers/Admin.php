1bc8b8089a8c63438cf842f69d3d9ff097854817
fyrsna <firda20240200119@sibermu.ac.id>
2025-10-29 21:00:46 +0700

    index dan admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @property CI_Form_validation $form_validation
+ * @property CI_Session $session
+ * @property CI_Input $input
+ * @property CI_DB_query_builder $db
+ * @property CI_URI $uri
+ * @property CI_Lang $lang
+ */
+class Admin extends CI_Controller 
+{
+    public function index()
+	{
+		$this->load->view('admin/index');
+	}
+}

612a0492343d28e2f4d70567b9ca09679cc7c6a9
fyrsna <firda20240200119@sibermu.ac.id>
2025-11-06 15:23:38 +0700

    perbaikan admin & index

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,6 +13,9 @@ class Admin extends CI_Controller
 {
     public function index()
 	{
-		$this->load->view('admin/index');
+		$data['title'] = 'My Profile';
+		$data ['admin'] = $this->db->get_where('user', ['email' => 
+		$this->session->userdata('email')])->row_array();
+		$this->load->view('admin/index', $data);
 	}
 }

adf42de5e1f1f0049bf820b57f825b80d3f8c2d2
fyrsna <firda20240200119@sibermu.ac.id>
2025-11-06 17:04:38 +0700

    header, footer, index dan admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,11 +11,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Admin extends CI_Controller 
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+        $this->load->database(); 
+    }
+    
     public function index()
-	{
-		$data['title'] = 'My Profile';
-		$data ['admin'] = $this->db->get_where('user', ['email' => 
-		$this->session->userdata('email')])->row_array();
-		$this->load->view('admin/index', $data);
-	}
+    {
+        $data['title'] = 'My Profile';
+        
+        $data ['admin'] = $this->db->get_where('user', ['email' => 
+            $this->session->userdata('email')])->row_array();
+            
+        $this->load->view('templates/header', $data);  
+        $this->load->view('admin/index', $data);       
+        $this->load->view('templates/footer');        
+    }
 }
